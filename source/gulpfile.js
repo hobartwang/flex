afc7724f143ac2c487b37767747685c2b3453426
Hobart Wang <hobart.cn@gmail.com>
2016-03-04 23:59:10 +0800

    gulp-wrap

--- source/gulpfile.js
+++ source/gulpfile.js
@@ -1,11 +1,18 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var prefix = require('gulp-autoprefixer');
+var wrap = require('gulp-wrap');
 
 function handleError(err) {
 	console.log(err.toString());
 	this.emit('end');
 }
+
+gulp.task('build',function(){
+	gulp.src("pages/*.html")
+		.pipe(wrap({src:"layout/default.html"}))
+		.pipe(gulp.dest(".."));
+})
 
 gulp.task('sass', function(){
   gulp.src('styles/main.scss')
